5f57bcecc223071f3567df19badc3749140d01f1
Berkay Kaya <80210343+imberkaykaya@users.noreply.github.com>
2023-11-26 00:41:57 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyDictionary
+{
+	internal class MyDictionary<T>
+	{
+		T[] items;
+		public MyDictionary()
+		{
+			items = new T[0]; //
+		}
+		public void Add(T item)
+		{
+			T[] tempArray = items; //geçici değişken 
+			items = new T[items.Length + 1]; //yeni gelecek değer --> dizi boyutunu bir arttırarak geliyor.
+
+			for (int i = 0; i < tempArray.Length; i++)
+			{
+				items[i] = tempArray[i]; //geçici değişkendeki verileri kendi dizimize tekrar alıyoruz.
+			}
+			items[items.Length - 1] = item; //atanacak yeni değeri son elemana eklemek için.
+		}
+
+		public int Length
+		{
+			get { return items.Length; }
+		}
+		public T[] Items
+		{
+			get { return items; }
+		}
+	}
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace MyDictionary
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			MyDictionary<string> Cars = new MyDictionary<string>();
+
+			Cars.Add("TOGG");
+			Cars.Add("TESLA");
+			Cars.Add("Renault");
+
+			foreach (var i in Cars.Items)
+			{
+				Console.WriteLine(i);
+			}
+		}
+	}
+}
